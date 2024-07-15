510dc358d30d3eb0c234afde7f5d27697f600e1c
rafael <enterrafael@gmail.com>
2024-07-15 15:47:58 -0300

    projeto web LH Pets

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4735500133e30a23de373420ff5b2de4faaeac25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da77484eafb203815adb61f52f3acd4fd886426")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

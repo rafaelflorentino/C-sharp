8e614d4fd63380fce773ded47c8494cc60ce89a3
rafael <enterrafael@gmail.com>
2024-10-04 17:23:31 -0300

    teste imc sem erros

--- AtividadeXUnit/atividade_imc_xunit/UnitTest1.cs
+++ AtividadeXUnit/atividade_imc_xunit/UnitTest1.cs
@@ -1,33 +1,35 @@
-namespace atividade_imc_xunit;
+using Xunit;
 
-public class UnitTest1
+namespace atividade_imc_xunit
 {
-    [Fact]
-    public void Teste_Calculo_IMC()
+    public class UnitTest1
     {
-        double imc_previsto = 31.25;
-        IMC i = new IMC();
-        i.peso = 80;
-        i.altura = 1.60;
-        i.Calcular_IMC();
-        Assert.Equal(imc_previsto, i.imc);
-    }
+        [Fact]
+        public void Teste_Calculo_IMC()
+        {
+            double imc_previsto = 31.25;
+            IMC i = new IMC();
+            i.peso = 80;
+            i.altura = 1.60;
+            i.Calcular_IMC();
+            Assert.Equal(imc_previsto, i.imc);
+        }
 
-    [Theory]
-    [InlineData("Abaixo do peso")]
-    [InlineData("Peso normal")]
-    [InlineData("Sobrepeso")]
-    [InlineData("Obesidade Grau I")]
-    [InlineData("Obesidade Grau II")]
-    [InlineData("Obesidade Grau III")]
-    public void Teste_Categoria_IMC(String cat)
-    {
-        IMC i = new IMC();
-        i.peso = 80;
-        i.altura = 1.60;
-        i.Calcular_IMC();
-        i.Classificar_IMC();
-        Assert.Equal(i.categoria, cat);
+        [Theory]
+        [InlineData(17, "Abaixo do peso")]
+        [InlineData(22, "Peso normal")]
+        [InlineData(27, "Sobrepeso")]
+        [InlineData(32, "Obesidade Grau I")]
+        [InlineData(37, "Obesidade Grau II")]
+        [InlineData(42, "Obesidade Grau III")]
+        public void Teste_Categoria_IMC(double imc, string categoriaEsperada)
+        {
+            IMC i = new IMC();
+            i.peso = 80; // Valor qualquer apenas para manter a estrutura
+            i.altura = 1.60; // Valor qualquer apenas para manter a estrutura
+            i.imc = imc;
+            i.Classificar_IMC();
+            Assert.Equal(categoriaEsperada, i.categoria);
+        }
     }
-
 }
